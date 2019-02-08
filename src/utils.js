08efa61e68dbe38486a4a038df1d7581c1be2a46
Viliam Simko <viliam.simko@gmail.com>
2019-02-08 10:37:33 +0100

    add doc-comments to functions inspired by ramda

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,26 @@
-// api same as ramda
+// @ts-check
+
+/** api same as ramda */
 const map = fn => list => list.map(fn)
+
+/** api same as ramda */
 const filter = fn => list => list.filter(fn)
+
+/** api same as ramda */
 const values = obj => Object.keys(obj).map(key => obj[key])
+
+/** api same as ramda */
 const length = strOrArray => (strOrArray != null ? strOrArray.length : 0)
+
+/** api same as ramda */
 const isString = x => x != null && x.constructor === String
 
+/** api same as ramda */
 const compose = (...fnlist) => data =>
   [...fnlist, data].reduceRight((prev, fn) => fn(prev))
 
+/** api same as ramda */
 const mapObjIndexed = fn => obj => {
-  // FIXME: sure, there is a better way to do this
-  obj = obj['where']
   const acc = {}
   Object.keys(obj).forEach(key => (acc[key] = fn(obj[key], key, obj)))
   return acc
